9dc1abb1b065acc13845873680d503d8f46bb9d2
MinahzParlight <giobien.light.1222@gmail.com>
2026-07-17 13:26:15 +0700

    update gitignore

--- Models/UserLogin.cs
+++
@@ -1,8 +0,0 @@
-namespace UserService.Models
-{
-    public class UserLogin
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
